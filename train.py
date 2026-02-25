698144b077d4087d9ce15ad0e5fc8ab01fb95e73
IrisOfElysium <Elysium17socials@gmail.com>
2026-02-25 12:17:44 +0100

    Dataset commit

--- train.py
+++ train.py
@@ -1,5 +1,20 @@
-import torch
+from sklearn.model_selection import train_test_split
 from torchvision.models import resnet101
-model = resnet101()
+import matplotlib
+import torch
+import PIL
 
+
+model = resnet101()
 print("Imported Model")
+
+CLASSES = 50
+EPOCHS = 50
+INITIAL_LR = 0.001
+BATCH_SIZE = 64
+
+
+dvc remote modify minioserver endpointurl http://172.24.198.42:9000
+dvc remote modify minioserver access_key_id daki
+dvc remote modify minioserver secret_access_key dakiminio
+dvc remote modify minioserver use_ssl false
